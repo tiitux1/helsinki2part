83982cc5b20022a38b6973af9f879509d4786fa7
tiitux1 <152963179+tiitux1@users.noreply.github.com>
2025-02-10 09:45:16 +0200

    2.11

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,49 +1,37 @@
-import { useState } from 'react';
-import Filter from './components/Filter';
-import PersonForm from './components/PersonForm';
-import Persons from './components/Persons';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
-  const [filter, setFilter] = useState('');
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
-  const addPerson = (event) => {
-    event.preventDefault();
-    const personObject = { name: newName, number: newNumber };
-
-    setPersons(persons.concat(personObject));
-    setNewName('');
-    setNewNumber('');
-  };
-
-  const filteredPersons = persons.filter(person =>
-    person.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   return (
     <div>
       <h2>Phonebook</h2>
-
       <Filter filter={filter} setFilter={setFilter} />
-
       <h3>Add a new</h3>
-      <PersonForm 
-        addPerson={addPerson} 
-        newName={newName} 
-        setNewName={setNewName} 
-        newNumber={newNumber} 
-        setNewNumber={setNewNumber} 
+      <PersonForm
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+        persons={persons}
+        setPersons={setPersons}
       />
-
       <h3>Numbers</h3>
-      <Persons persons={filteredPersons} />
+      <Persons persons={persons} filter={filter} />
     </div>
   );
 };
