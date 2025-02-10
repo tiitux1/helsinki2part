3b9e01a9a68c2e5e95fb68aeca14e50d401262c5
tiitux1 <152963179+tiitux1@users.noreply.github.com>
2025-02-10 09:18:22 +0200

    2.8

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,35 +1,46 @@
-import Course from './Course'
+import { useState } from 'react';
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        { name: 'Fundamentals of React', exercises: 10, id: 1 },
-        { name: 'Using props to pass data', exercises: 7, id: 2 },
-        { name: 'State of a component', exercises: 14, id: 3 },
-        { name: 'Redux', exercises: 11, id: 4 }
-      ]
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        { name: 'Routing', exercises: 3, id: 1 },
-        { name: 'Middlewares', exercises: 7, id: 2 }
-      ]
-    }
-  ]
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      number: newNumber
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName('');
+    setNewNumber('');
+  };
 
   return (
     <div>
-      <h1>Web development curriculum</h1>
-      {courses.map(course => (
-        <Course key={course.id} course={course} />
-      ))}
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>
+            {person.name} {person.number}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
