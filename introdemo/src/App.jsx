0c3b85bf9b69b08face361a57923e12083961026
tiitux1 <152963179+tiitux1@users.noreply.github.com>
2025-02-10 09:18:41 +0200

    2.9

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,25 +1,39 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
-    const personObject = {
-      name: newName,
-      number: newNumber
-    };
+    const personObject = { name: newName, number: newNumber };
 
     setPersons(persons.concat(personObject));
     setNewName('');
     setNewNumber('');
   };
 
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      {/* Hakukenttä */}
+      <div>
+        Filter shown with: <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      </div>
+
+      <h3>Add a new</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
@@ -31,9 +45,10 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+
+      <h3>Numbers</h3>
       <ul>
-        {persons.map((person, index) => (
+        {filteredPersons.map((person, index) => (
           <li key={index}>
             {person.name} {person.number}
           </li>
