15335fa938903c2407830bf9f29b5835dec43f18
tiitux1 <152963179+tiitux1@users.noreply.github.com>
2025-03-03 10:18:32 +0200

    2.12

--- introdemo/src/components/PersonForm.jsx
+++ introdemo/src/components/PersonForm.jsx
@@ -1,18 +1,41 @@
-const PersonForm = ({ addPerson, newName, setNewName, newNumber, setNewNumber }) => {
-    return (
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-    );
+import axios from "axios";
+
+const PersonForm = ({ newName, setNewName, newNumber, setNewNumber, persons, setPersons }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already in the phonebook!`);
+      return;
+    }
+
+    const newPerson = { name: newName, number: newNumber };
+
+    axios
+      .post("http://localhost:3001/persons", newPerson)  // 🔥 Lähetetään palvelimelle
+      .then(response => {
+        setPersons(persons.concat(response.data));  // 🔄 Päivitetään tila uudella henkilöllä
+        setNewName("");
+        setNewNumber("");
+      })
+      .catch(error => {
+        console.error("Error adding person:", error);
+      });
   };
-  
-  export default PersonForm;
-  
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        Name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+      </div>
+      <div>
+        Number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+      </div>
+      <div>
+        <button type="submit">Add</button>
+      </div>
+    </form>
+  );
+};
+
+export default PersonForm;
